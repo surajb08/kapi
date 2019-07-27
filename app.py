6f39e636b931306adc6d5f32b866e3e013ec0817
Dan Octavian <danoctavian91@gmail.com>
2019-07-27 14:33:47 +0800

    add deployment and namespaces endpoints

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
-
-import requests
-from flask import Flask, request, jsonify
-from typing import List
+from flask import Flask, request, make_response, jsonify
+from http import HTTPStatus
 import os
 import json
+from kubernetes import client, config
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+coreV1 = client.CoreV1Api()
+extensionsV1Beta = client.ExtensionsV1beta1Api()
+
+extensions = client.ExtensionsApi()
+
+ret = coreV1.list_namespace()
+
 
 HOST = '0.0.0.0'
 PORT = 5000
@@ -27,13 +37,86 @@ def simple_deployment(hash):
         
     }
 
+@app.route('/api/namespaces/<namespace>/deployments/<deployment_name>', methods=['GET'])
+def get_namespaced_deployment(namespace, deployment_name):
+    response = extensionsV1Beta.list_namespaced_deployment(namespace)
+    items = response.items
+    matches = list([item for item in items if item.metadata.name == deployment_name])
+    if len(matches) == 0:
+        make_response({ "message": "Deployment not found"}, HTTPStatus.NOT_FOUND)
+
+    # kuberenetes names are unique: there will be only 1 deployment if any
+    deployment = matches[0]
+    isodate = deployment.metadata.creation_timestamp.isoformat()
+
+    containers = []
+    for container in deployment.spec.template.spec.containers:
+        returned_ports = [{"number": port.container_port, "type": port.protocol} for port in container.ports]
+
+        resources = []
+        resource_requests = deployment.spec.template.spec.containers[0].resources.requests
+        for key in resource_requests.keys():
+            resources.append({
+                "type": key,
+                "requirement": resource_requests[key]
+            })
+
+        returned_container = {
+            "name": container.name,
+            "image": container.image,
+            "ports": returned_ports,
+            "resources": resources
+        }
+
+        containers.append(returned_container)
+
+    return {
+        "name": deployment.metadata.name,
+        "namespace": namespace,
+        # "language": String,
+        # "githubRepo": String,
+        "createdAt": isodate,
+        "containers": containers,
+        # endpoints: {
+        #     external: {
+        #         host: String,
+        #         port: Int,
+        #         type: String,
+        #     },
+        #     internal: {
+        #         host: String,
+        #         port: Int,
+        #         type: String,
+        #     },
+        # },
+        # pods: [{
+        #     id: String
+        #     status: String, ('active', 'inactive')
+        # }],
+        #"labels": [{id: String, value: String}]
+    }
+
+
+@app.route('/api/namespaces', methods=['GET'])
+def get_namespaces():
+    response = coreV1.list_namespace()
+    items = response.items
+    namespaces = list([{"name": namespace.metadata.name} for namespace in items])
+    return {
+        "items": namespaces,
+        "total": len(namespaces)
+    }
+
+
 @app.route('/')
 def hello():
     return "<h1>Hello worlds</h1>"
 
+
 @app.route('/yo')
 def test():    
     return kmd("get all")
+
 
 @app.route('/api/deployments/all', methods=['GET'])
 def get_deployments_all():
