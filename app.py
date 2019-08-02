44667301ff0b500345d0365f4d0ea86df9e896a2
Dan Octavian <danoctavian91@gmail.com>
2019-08-02 20:09:48 +0800

    add code from pyxterm

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, render_template
 from flask_cors import CORS
 from http import HTTPStatus
 import os
@@ -8,11 +8,25 @@ import utils
 from kube_deployment import get_deployment_details
 from kube_apis import coreV1, extensionsV1Beta
 
+# container ssh specific imports
+from flask_socketio import SocketIO
+import pty
+import os
+import subprocess
+import select
+import termios
+import struct
+import fcntl
+import shlex
+
 
 HOST = '0.0.0.0'
 PORT = 5000
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".", static_folder=".", static_url_path="")
+app.config["SECRET_KEY"] = "secret!"
+app.config["fd"] = None
+app.config["child_pid"] = None
 CORS(app)
 
 
@@ -120,6 +134,78 @@ def get_namespaces():
 def hello():
     return "<h1>Hello worlds</h1>"
 
+socketio = SocketIO(app)
+
+def set_winsize(fd, row, col, xpix=0, ypix=0):
+    winsize = struct.pack("HHHH", row, col, xpix, ypix)
+    fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
+
+
+def read_and_forward_pty_output():
+    max_read_bytes = 1024 * 20
+    while True:
+        socketio.sleep(0.01)
+        if app.config["fd"]:
+            timeout_sec = 0
+            (data_ready, _, _) = select.select([app.config["fd"]], [], [], timeout_sec)
+            if data_ready:
+                output = os.read(app.config["fd"], max_read_bytes).decode()
+                socketio.emit("pty-output", {"output": output}, namespace="/pty")
+
+
+@app.route("/containersshpage")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on("pty-input", namespace="/pty")
+def pty_input(data):
+    """write to the child pty. The pty sees this as if you are typing in a real
+    terminal.
+    """
+    if app.config["fd"]:
+        # print("writing to ptd: %s" % data["input"])
+        os.write(app.config["fd"], data["input"].encode())
+
+
+@socketio.on("resize", namespace="/pty")
+def resize(data):
+    if app.config["fd"]:
+        set_winsize(app.config["fd"], data["rows"], data["cols"])
+
+
+@socketio.on("connect", namespace="/pty")
+def connect():
+    """new client connected"""
+
+    if app.config["child_pid"]:
+        # already started child process, don't start another
+        return
+
+    # create child process attached to a pty we can read from and write to
+    (child_pid, fd) = pty.fork()
+    if child_pid == 0:
+        # this is the child process fork.
+        # anything printed here will show up in the pty, including the output
+        # of this subprocess
+        subprocess.run(app.config["cmd"])
+    else:
+        # this is the parent process fork.
+        # store child fd and pid
+        app.config["fd"] = fd
+        app.config["child_pid"] = child_pid
+        set_winsize(fd, 50, 50)
+        cmd = " ".join(shlex.quote(c) for c in app.config["cmd"])
+        print("child pid is", child_pid)
+        print(
+            f"starting background task with command `{cmd}` to continously read "
+            "and forward pty output to client"
+        )
+        socketio.start_background_task(target=read_and_forward_pty_output)
+        print("task started")
+
 
 if __name__ == '__main__':
+    app.config["cmd"] = ["bash"]
+    socketio.run(app, debug=True, port=PORT)
     app.run(host=HOST, debug=True, port=PORT)
