dbd0f375482fb7720432be91d9dedc95f772bf28
xavier <xavier.r.millot@gmail.com>
2019-08-24 17:28:41 +0100

    white/black list filtering working

--- dep_helper.py
+++ dep_helper.py
@@ -7,7 +7,7 @@ class DepHelper:
   def find(namespace, name):
     return broker.appsV1Api.read_namespaced_deployment(
       namespace=namespace,
-      name=name,
+      name=name
     )
 
   @staticmethod
@@ -15,3 +15,25 @@ class DepHelper:
     match_labels = deployment.spec.selector.match_labels
     returned_services = broker.coreV1.list_service_for_all_namespaces()
     return list(filter(lambda x: x.spec.selector == match_labels, returned_services.items))
+
+  @staticmethod
+  def ns_whitelist(whitelist, deps = None):
+    disc = lambda d: d.metadata.namespace in whitelist
+    return list(filter(disc, deps))
+
+  @staticmethod
+  def ns_blacklist(blacklist, deps = None):
+    disc = lambda d: d.metadata.namespace not in blacklist
+    return list(filter(disc, deps))
+
+  @staticmethod
+  def ns_filter(filters, _type='whitelist'):
+    deps = broker.appsV1Api.list_deployment_for_all_namespaces().items
+    # print(deps)
+    method = DepHelper.ns_whitelist if _type == 'whitelist' else DepHelper.ns_blacklist
+    return method(filters, deps)
+
+# broker.connect()
+# res = DepHelper.ns_filter(['kube-system', None], "blacklist")
+# print(res)
+# print(len(res))
