aec2a4ba4bfc379aa9c79136a4c9f68b4a06bf16
xavier <xavier@codenectar.com>
2019-11-18 23:42:50 +0100

    all pod checks done not tested

--- k8_kat/events/kat_event.py
+++ k8_kat/events/kat_event.py
@@ -20,5 +20,25 @@ class KatEvent:
   def for_name(self):
     return self.raw.involved_object.name
 
+  @property
+  def reason(self):
+    return self.raw.reason
+
+  @property
+  def message(self):
+    return self.message
+
   def is_for(self, kat_res):
     return self.for_uid == kat_res.uid
+
+  def is_config_map_err(self):
+    return "configmaps" in self.message
+
+  def is_secrets_err(self):
+    return "with: secrets" in self.message
+
+  def is_probe_err(self):
+    return self.reason == 'Unhealthy'
+
+  def is_insufficient_cpu(self):
+    return "Insufficient cpu" in self.message
--- k8_kat/pod/pod_solver.py
+++ k8_kat/pod/pod_solver.py
@@ -1,4 +1,42 @@
 
 class PodSolver:
-  def __init__(self, pod):
-    self.pod = pod
+
+  @staticmethod
+  def solve(pod):
+    events = pod.events()
+
+  @staticmethod
+  def is_img_pull_bkf(pod, _):
+    return pod.status == 'ImagePullBackOff'
+
+  @staticmethod
+  def is_crash_loop_bkf(pod, _):
+    return pod.status == 'CrashLoopBackoff'
+
+  @staticmethod
+  def is_bad_config_map(pod, events):
+    if pod.status == 'RunContainerError':
+      suspects = [e for e in events if e.is_config_map_err()]
+      return len(suspects) > 0
+    return False
+
+  @staticmethod
+  def is_bad_secrets(pod, events):
+    if pod.status == 'ContainerCreating':
+      suspects = [e for e in events if e.is_config_map_err()]
+      return len(suspects) > 0
+    return False
+
+  @staticmethod
+  def is_failing_probe(pod, events):
+    if pod.status == 'Running':
+      suspects = [e for e in events if e.is_probe_err()]
+      return len(suspects) > 0
+    return False
+
+  @staticmethod
+  def is_insufficient_cpu(pod, events):
+    if pod.status == 'Running':
+      suspects = [e for e in events if e.is_insufficient_cpu()]
+      return len(suspects) > 0
+    return False
